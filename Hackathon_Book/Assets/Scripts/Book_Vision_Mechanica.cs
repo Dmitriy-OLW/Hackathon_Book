7d7f4303d2f1ee6a3c042b704c6bccabc4422bbc
Dmitriy-OLW <124800644+Dmitriy-OLW@users.noreply.github.com>
2024-04-21 15:02:16 +0300

    Fast_Start_Hack
    Plagins_Assets
    Book_Vision
    First_Person_Controler

---
+++ Hackathon_Book/Assets/Scripts/Book_Vision_Mechanica.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Book_Vision_Mechanica : MonoBehaviour
+{
+    public KeyCode world_0 = KeyCode.Keypad1;
+    public KeyCode world_1 = KeyCode.Keypad2;
+    //public KeyCode world_2 = KeyCode.Keypad3;
+    public GameObject[] Mask_Vid_World;
+    void Start()
+    {
+        Mask_Vid_World[0] = GameObject.Find("Mask0");
+        Mask_Vid_World[1] = GameObject.Find("Mask1");
+        //Mask_Vid_World[2] = GameObject.Find("Mask2");
+        foreach(GameObject _mask in Mask_Vid_World){Deactivate_Activate_World(_mask, false);}
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(world_0)){
+           mask_on_off(Mask_Vid_World, 0);
+        }
+        if(Input.GetKeyDown(world_1)){
+           mask_on_off(Mask_Vid_World, 1);
+        }
+         //if(Input.GetKeyDown(world_2)){
+           // mask_on_off(Mask_Vid_World, 2);
+         //}
+    }
+    void mask_on_off(GameObject[] Masks, int Nomer)
+    {
+        foreach(GameObject _mask in Masks){Deactivate_Activate_World(_mask, false);}
+        Deactivate_Activate_World(Masks[Nomer], true);
+    }
+    void Deactivate_Activate_World(GameObject Objects, bool b)
+    {
+        Objects.SetActive(b);
+    }
+}
