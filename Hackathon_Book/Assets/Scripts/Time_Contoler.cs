fabe52bca133347b7de311685ccb42292a8ad07d
Dmitriy-OLW <124800644+Dmitriy-OLW@users.noreply.github.com>
2024-04-27 00:40:41 +0300

    time_Controller

---
+++ Hackathon_Book/Assets/Scripts/Time_Contoler.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using TMPro;
+public class Time_Contoler : MonoBehaviour
+{
+    public KeyCode _T = KeyCode.Alpha0;
+   [SerializeField] private float time;
+    [SerializeField] private TextMeshProUGUI timerText;
+ 
+    private float _timeLeft = 0f;
+    private float _Nachal_Time;
+    private void Start() {
+        StartTime();
+    }
+    private void Update() {
+        if (Input.GetKeyDown(_T)) StartTime();
+    }
+    private IEnumerator StartTimer()
+    {
+        while (_timeLeft > 0)
+        {
+            _timeLeft -= Time.deltaTime;
+            UpdateTimeText();
+            yield return null;
+        }
+    }
+ 
+    public void StartTime()
+    {
+        _Nachal_Time = time;
+        _timeLeft = time;
+        StartCoroutine(StartTimer());
+    }
+ 
+    private void UpdateTimeText()
+    {
+        if (_timeLeft < 0)
+            _timeLeft = 0;
+
+        float minutes = Mathf.FloorToInt(_timeLeft / 60);
+        float seconds = Mathf.FloorToInt(_timeLeft % 60);
+        timerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+        if(_Nachal_Time < _timeLeft){timerText.text = "Wave Begun";}
+        if(_timeLeft <= 0)
+        {
+            timerText.text = "You Dead"; 
+        }
+        
+
+    }
+
+}
