276ae315ae7b0fbca9d515c2a348dd38daae72c2
Ксения Нагишева <knagisheva@gmail.com>
2024-04-27 08:33:47 +0300

    Начало настроек

--- Hackathon_Book/Assets/Scripts/MenuButtons.cs
+++ Hackathon_Book/Assets/Scripts/MenuButtons.cs
@@ -5,15 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
+
+    [SerializeField] GameObject Main_Cam;
+    [SerializeField] GameObject Nast_Cam;
+
+    [SerializeField] GameObject Main_Canv;
+    [SerializeField] GameObject Nast_Canv;
     public void Play()
     {
         SceneManager.LoadScene(1);
     }
 
 
-    public void Nastr()
+    public void ToNastr()
     {
-        //
+        Main_Canv.SetActive(false);
+        Main_Cam.SetActive(false);
+
+        Nast_Canv.SetActive(true);
+        Nast_Cam.SetActive(true);
+    }
+
+    public void FromNastr()
+    {
+        Nast_Canv.SetActive(false);
+        Nast_Cam.SetActive(false);
+
+        Main_Canv.SetActive(true);
+        Main_Cam.SetActive(true);
     }
 
     public void Exit()
